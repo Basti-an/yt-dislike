25cdb63e8b63f1353f79bbbf9f74eb2f786c137c
Sebastian Wiendlocha <sebastian@wiendlocha.org>
2021-11-25 16:45:22 +0100

    ✨ implement oAuth2

--- src/background.ts
+++ src/background.ts
@@ -1,4 +1,4 @@
-const API_KEY = "your-api-key-here";
+const API_KEY = process.env.API_KEY;
 
 chrome.runtime.onMessage.addListener(function (msg, sender, sendResponse) {
   if (!msg.video_id) {
@@ -6,12 +6,25 @@ chrome.runtime.onMessage.addListener(function (msg, sender, sendResponse) {
     return;
   }
 
-  const response = fetch(
-    `https://www.googleapis.com/youtube/v3/videos?key=${API_KEY}&id=${msg.video_id}&part=statistics`
-  ).then((response) => {
-    response.json().then((body) => {
-      const dislikes = body.items[0].statistics.dislikeCount;
-      sendResponse({ dislikes });
+  chrome.identity.getAuthToken({ interactive: true }, function (token) {
+    let init = {
+      method: "GET",
+      async: true,
+      headers: {
+        Authorization: "Bearer " + token,
+        "Content-Type": "application/json",
+      },
+      contentType: "json",
+    };
+
+    fetch(
+      `https://www.googleapis.com/youtube/v3/videos?key=${API_KEY}&id=${msg.video_id}&part=statistics`,
+      init
+    ).then((response) => {
+      response.json().then((body) => {
+        const dislikes = body.items[0].statistics.dislikeCount;
+        sendResponse({ dislikes });
+      });
     });
   });
 
